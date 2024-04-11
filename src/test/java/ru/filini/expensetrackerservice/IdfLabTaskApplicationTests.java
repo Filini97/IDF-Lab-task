05a947331edaaf4b5248bf5eef853ce42a357521
Alex-Filini <Filini97@yandex.ru>
2024-04-11 16:04:39 +0300

    Initial commit

---
+++ src/test/java/ru/filini/expensetrackerservice/IdfLabTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.filini.expensetrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfLabTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
