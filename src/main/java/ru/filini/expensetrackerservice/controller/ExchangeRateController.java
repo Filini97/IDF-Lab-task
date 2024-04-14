c49148cdb35e7b4fee9197156aa7403d8f4e64d0
Alex-Filini <Filini97@yandex.ru>
2024-04-14 19:18:08 +0300

    ExchangeRateService and ExchangeRateController realized.

--- src/main/java/ru/filini/expensetrackerservice/controller/ExchangeRateController.java
+++ src/main/java/ru/filini/expensetrackerservice/controller/ExchangeRateController.java
@@ -1,4 +1,31 @@
 package ru.filini.expensetrackerservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.filini.expensetrackerservice.service.ExchangeRateService;
+
+@RestController
+@RequestMapping("/exchange-rates")
 public class ExchangeRateController {
+
+    private final ExchangeRateService exchangeRateService;
+
+    @Autowired
+    public ExchangeRateController(ExchangeRateService exchangeRateService) {
+        this.exchangeRateService = exchangeRateService;
+    }
+
+    @GetMapping("/fetch-and-save")
+    public ResponseEntity<String> fetchAndSaveExchangeRates() {
+        try {
+            exchangeRateService.fetchAndSaveExchangeRates();
+            return ResponseEntity.ok("Exchange rates fetched and saved successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to fetch and save exchange rates");
+        }
+    }
 }
